3384df74c9355489065de6b7f68908b32e019c95
Debora Maria Rosa Siqueira <14549327677@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-02-16 10:40:08 -0300

    Carga Inicial

---
+++ aula1/exemplo1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        for($c = 1; $c <=50; $c = $c +1){
+    ?>
+        <img src="https://tm.ibxk.com.br/2021/05/03/03183011251371.jpg?ims=1200x675"
+        alt="Imagem do homem aranha"
+        width="10%">
+        
+    <?php } ?>
+        
+
+</body>
+</html>
---
+++ aula2/variaveis.php
@@ -0,0 +1,22 @@
+<h2> Variáveis </h2>
+<p>
+    São estruturas (memória) utilizadas para armazenar
+    dados que serão processados pelo programa
+</p>
+
+<p>
+    Para criar uma variável em PHP basta colocar simbolo de $ na
+    frente de um nome e atribuir um valor com o simbolo de igual. Exemplo:
+
+    <pre>
+        $variavel = 10;
+        $nome = "Maria";
+        $nota = 5.5;<>
+    </pre>
+
+</p>
+
+<?php
+    $nome = "Débora";
+    echo "Olá $nome, seja bem vindo a aula de backend";
+?>
