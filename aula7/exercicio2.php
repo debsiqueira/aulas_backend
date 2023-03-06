807ca23869dd14fb1596faa68146b027716d5d93
Debora Maria Rosa Siqueira <14549327677@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-03-06 08:44:07 -0300

    exercicio Vetores - ordem

---
+++ aula7/exercicio2.php
@@ -0,0 +1,38 @@
+<h2>Exercício 2</h2>
+
+<p>
+    Crie um vetor com os nomes, conforme exemplo abaixo:
+        <pre>
+          --------- 
+       0 |Murilo   | 
+          --------- 
+       1 |Poliana  |
+          --------- 
+       2 |Dalva    |
+          --------- 
+       3 |Lavinia  |
+          --------- 
+       4 |Kaua     |
+          --------- 
+    </pre>
+</p>
+<p>
+    Imprima os nomes do vetor em formato de lista ordenada: ex:
+        <br>
+        1. Murilo <br>
+        2. Poliana <br>
+        3. .... <br>
+</p>
+
+<hr>
+
+<?php
+   $nomes=["Murilo", "Poliana", "Dalva", "Lavinia", "Kaua"];
+    
+   echo "<ol>";
+   for($f=0; $f<5; $f++){
+    echo "<li>$nomes[$f]</li>";  
+   }
+   "</ol>"
+   
+?>
