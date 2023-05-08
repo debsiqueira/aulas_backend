23dd46581f867d2fd6e60f634861a1f785660926
Debora Maria Rosa Siqueira <14549327677@DC01.IFS.IFSULDEMINAS.EDU.BR>
2023-05-08 08:37:35 -0300

    aula 18

--- aula18/produto/inserir.php
+++ aula18/produto/inserir.php
@@ -2,6 +2,7 @@
 
 require_once "../conexao.php";
 
+if(isset($_POST["nome"]) && isset($_POST["descricao"]) && isset($_POST["preco"])){
 $nome = $_POST["nome"];
 $descricao = $_POST["descricao"];
 $preco = $_POST["preco"];
@@ -16,10 +17,12 @@ echo $sql;
 $comando = $conexao->prepare($sql);
 
 //adiciona os valores nos parâmetros
-$comando -> bind_param("ssds", $nome,$descricao, $preco, $foto);
+$comando -> bind_param("ssds ", $nome,$descricao, $preco, $foto);
 
 //executa o SQL - Comando no Banco de Dados
 $comando->execute();
 
+}
 //abre o arquivo form.php
-//header("Location: form.php");
+header("Location: form.php");
+
